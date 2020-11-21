3f3844fa744bf2a0730bfc78f9f4af244d974953
heathkev <kevintuazon45@gmail.com>
2020-11-22 02:34:07 +0800

    Extracted an authform component in sign up

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,44 +1,28 @@
-import React, { useState, useContext } from 'react'
-import { View, StyleSheet, KeyboardAvoidingView, ScrollView } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements'
+import React, { useContext } from 'react'
+import { View, StyleSheet, KeyboardAvoidingView, ScrollView, TouchableOpacity } from 'react-native';
+import { Text } from 'react-native-elements'
+import AuthForm from '../components/AuthForm';
 import Spacer from '../components/Spacer'
 import { Context as AuthContext } from '../context/AuthContext'
 
 const SignupScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
     return (
         <KeyboardAvoidingView>
             <ScrollView style={styles.contentContainerStyle}>
                 <View style={styles.container}>
-                    <Spacer>
-                        <Text h3>Sign up for Tracker</Text>
-                    </Spacer>
-
-                    <Input
-                        label="Email"
-                        value={email}
-                        onChangeText={setEmail}
-                        autoCapitalize="none"
-                        autoCorrect={false}
+                    <AuthForm
+                        headerText="Sign Up for Tracker"
+                        errorMessage={state.errorMessage}
+                        submitButtonText="Sign Up"
+                        onSubmit={signup}
                     />
-                    <Spacer />
-                    <Input
-                        label="Password"
-                        secureTextEntry
-                        value={password}
-                        onChangeText={setPassword}
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                    />
-                    {state.errorMessage ? (
-                        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-                    ) : null}
-                    <Spacer>
-                        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-                    </Spacer>
+                    <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                        <Spacer>
+                            <Text style={styles.link}>Already have an account? Sign in instead</Text>
+                        </Spacer>
+                    </TouchableOpacity>
                 </View>
             </ScrollView>
         </KeyboardAvoidingView>
@@ -60,11 +44,8 @@ const styles = StyleSheet.create({
     contentContainerStyle: {
         paddingVertical: 150,
     },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15
+    link: {
+        color: 'blue'
     }
 });
 
