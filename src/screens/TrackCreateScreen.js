5d0f3ac0659b34d0d8d5fba8105afbaf579acd90
heathkev <kevintuazon45@gmail.com>
2020-12-01 00:14:02 +0800

    creates a new track form component added new types and functions updates implementation in track create screen

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -6,16 +6,19 @@ import { Text } from 'react-native-elements'
 import Map from '../components/Map'
 import { Context as LocationContext } from '../context/LocationContext'
 import useLocation from '../hooks/useLocation'
+import TrackForm from '../components/TrackForm'
 
 const TrackCreateScreen = ({ isFocused }) => {
-    const { addLocation } = useContext(LocationContext);
-    const [error] = useLocation(isFocused, addLocation);
+    const { state, addLocation } = useContext(LocationContext);
+    const [error] = useLocation(isFocused, (location) => {
+        addLocation(location, state.recording);
+    });
     
     return (
         <SafeAreaView forceInset={{ top: "always" }}>
             <Text style={{ fontSize: 48 }}>Create Track</Text>
             <Map />
-
+            <TrackForm />
             {error ? <Text>Please enable location services</Text> : null}
         </SafeAreaView>
     );
